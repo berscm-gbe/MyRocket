e9025c84b3c800c00d455882052d539084279a06
berscm-gbe <bernhard.schmitt@dentsplysirona.com>
2017-01-06 13:27:20 +0100

    Fixed namespace

--- MyRocket/MyRocket/IRocket.cs
+++ MyRocket/MyRocket/IRocket.cs
@@ -1,27 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Media.Media3D;
+﻿
 
 namespace MyRocket
 {
+    using System.Windows.Media.Media3D;
+
+    /// <summary>
+    /// Interface to access a flyable rocket.
+    /// </summary>
     public interface IRocket
     {
+        /// <summary>
+        /// Gets the flying state.
+        /// </summary>
+        /// <value>
+        /// The current flying state.
+        /// </value>
         FlyState State { get; }
 
+        /// <summary>
+        /// Gets the current flying position.
+        /// </summary>
+        /// <value>
+        /// The current flying position.
+        /// </value>
         Point3D CurrentPosition { get; }
 
+        /// <summary>
+        /// Gets the distance to destination.
+        /// </summary>
+        /// <value>
+        /// The distance to destination in [lightyears].
+        /// </value>
         uint DistanceToDestination { get; }
 
+        /// <summary>
+        /// Gets the remaining seconds to destination.
+        /// </summary>
+        /// <value>
+        /// The seconds to destination.
+        /// </value>
         uint SecondsToDestination { get; }
 
+        /// <summary>
+        ///Call me to prepare the rocket before the flight.
+        /// </summary>
+        /// <param name="fuel">The amount of fuel.</param>
+        /// <param name="destination">The destination.</param>
         void Prepare(uint fuel, Point3D destination);
 
+        /// <summary>
+        /// Tries to start the rocket.
+        /// </summary>
+        /// <returns>true, if starting sequence was successful; otherwise false.</returns>
         bool TryStart();
     }
 
+    /// <summary>
+    /// The allowed fyling states.
+    /// </summary>
     public enum FlyState
     {
         Initial,
--- MyRocket/MyRocket/Rocket.cs
+++ MyRocket/MyRocket/Rocket.cs
@@ -1,39 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Media.Media3D;
+﻿
 
 namespace MyRocket
 {
+    using System;
+    using System.Windows.Media.Media3D;
+
+    /// <summary>
+    /// Sample implementation of a virtual rocket.
+    /// </summary>
     public class Rocket : IRocket
     {
+        /// <summary>
+        /// Gets the flying state.
+        /// </summary>
+        /// <value>
+        /// The current flying state.
+        /// </value>
+        /// <exception cref="System.NotImplementedException"></exception>
         public FlyState State
         {
             get { throw new NotImplementedException(); }
         }
 
+        /// <summary>
+        /// Gets the current flying position.
+        /// </summary>
+        /// <value>
+        /// The current flying position.
+        /// </value>
+        /// <exception cref="System.NotImplementedException"></exception>
         public Point3D CurrentPosition
         {
             get { throw new NotImplementedException(); }
         }
 
+        /// <summary>
+        /// Gets the distance to destination.
+        /// </summary>
+        /// <value>
+        /// The distance to destination in [lightyears].
+        /// </value>
+        /// <exception cref="System.NotImplementedException"></exception>
         public uint DistanceToDestination
         {
             get { throw new NotImplementedException(); }
         }
 
+        /// <summary>
+        /// Gets the remaining seconds to destination.
+        /// </summary>
+        /// <value>
+        /// The seconds to destination.
+        /// </value>
+        /// <exception cref="System.NotImplementedException"></exception>
         public uint SecondsToDestination
         {
             get { throw new NotImplementedException(); }
         }
 
+        /// <summary>
+        /// Call me to prepare the rocket before the flight.
+        /// </summary>
+        /// <param name="fuel">The amount of fuel.</param>
+        /// <param name="destination">The destination.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
         public void Prepare(uint fuel, Point3D destination)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Tries to start the rocket.
+        /// </summary>
+        /// <returns>
+        /// true, if starting sequence was successful; otherwise false.
+        /// </returns>
+        /// <exception cref="System.NotImplementedException"></exception>
         public bool TryStart()
         {
             throw new NotImplementedException();
