ebd702a7e9d7ac4c30e7fb20ddc179e25d84aaeb
berscm-gbe <bernhard.schmitt@dentsplysirona.com>
2017-01-06 11:06:39 +0100

    + beef
    ... but still no implementation

---
+++ MyRocket/MyRocket/IRocket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace MyRocket
+{
+    public interface IRocket
+    {
+        FlyState State { get; }
+
+        Point3D CurrentPosition { get; }
+
+        uint DistanceToDestination { get; }
+
+        uint SecondsToDestination { get; }
+
+        void Prepare(uint fuel, Point3D destination);
+
+        bool TryStart();
+    }
+
+    public enum FlyState
+    {
+        Initial,
+        Preparing,
+        Starting,
+        Flying,
+        Landing,
+        Landed,
+        Crashed
+    }
+}
